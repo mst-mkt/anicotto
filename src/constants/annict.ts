7a4d836b289d0a8e48596a50ec7454a7e28a3265
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-02 05:52:01 +0900

    fix: remove baseurl for rest

--- src/constants/annict.ts
+++ src/constants/annict.ts
@@ -1,3 +1,2 @@
-const ANNICT_API_BASEURL = 'https://api.annict.com'
+export const ANNICT_API_BASEURL = 'https://api.annict.com'
 export const ANNICT_OAUTH_BASEURL = `${ANNICT_API_BASEURL}/oauth`
-export const ANNICT_API_V1_BASEURL = `${ANNICT_API_BASEURL}/v1`
