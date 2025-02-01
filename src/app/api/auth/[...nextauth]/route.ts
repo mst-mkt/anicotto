3b1ec9a1b35fa9beaaa2c642b0e538313de7c1a8
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-02 02:04:25 +0900

    fix(auth): wrap handlers to prevent error of opennextjs-cloudflare

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,13 @@
-import { handlers } from '../../../../lib/auth'
+import type { NextRequest } from 'next/server'
 
-export const { GET, POST } = handlers
+// Wrapping the handler because an error will occur if it is not a dynamic import.
+
+export const GET = async (request: NextRequest) => {
+  const { handlers } = await import('../../../../lib/auth')
+  return handlers.GET(request)
+}
+
+export const POST = async (request: NextRequest) => {
+  const { handlers } = await import('../../../../lib/auth')
+  return handlers.POST(request)
+}
