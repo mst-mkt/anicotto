00fb300b3dd5f2010b6746369b1ad0252cd7932c
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-05 09:56:30 +0900

    add: constants of project info

---
+++ src/constants/project.ts
@@ -0,0 +1,7 @@
+export const PROJECT_ID = 'anicotto'
+export const PROJECT_NAME = 'Anicotto'
+export const PROJECT_DESCRIPTION = 'Third-party Annict web client'
+export const PROJECT_AUTHOR = ['mst-mkt'] as const
+export const PROJECT_OWNER = 'mst-mkt'
+export const PROJECT_REPOSITORY_URL = `https://github.com/${PROJECT_OWNER}/${PROJECT_ID}`
+export const PROJECT_LINKS = [PROJECT_REPOSITORY_URL, 'https://annict.com/'] as const
