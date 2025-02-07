051ef3cd12db144c7abcd07f3d179994a56a9c9d
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-07 18:41:43 +0900

    fix: icon paths in manifest to use absolute urls

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,13 @@ const manifest = () =>
       {
         purpose: 'maskable',
         sizes: '512x512',
-        src: 'icon512_maskable.png',
+        src: '/icon512_maskable.png',
         type: 'image/png',
       },
       {
         purpose: 'any',
         sizes: '512x512',
-        src: 'icon512_rounded.png',
+        src: '/icon512_rounded.png',
         type: 'image/png',
       },
     ],
