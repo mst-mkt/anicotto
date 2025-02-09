21e38077553a09266715eb5903e78c94d90f6ee7
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-09 23:46:16 +0900

    fix: exclude manifest.json from redirect when no session

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,5 +9,5 @@ export const middleware = async () => {
 }
 
 export const config = {
-  matcher: ['/((?!login|api|_next|favicon.ico|robots.txt|sitemap.xml|.*.png).*)'],
+  matcher: ['/((?!login|api|_next|favicon.ico|robots.txt|manifest.json|sitemap.xml|.*.png).*)'],
 } satisfies MiddlewareConfig
