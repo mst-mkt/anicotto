2e1e9f3824be4486fb09d51189bdf840a99e77c4
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-10 13:32:16 +0900

    fix(styles): add padding to top of footer

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { Activities, ActivitiesLoading } from './_activity/activities'
 import { CurrentSeasonWork, CurrentSeasonWorkSkeleton } from './_current-season/current-season'
 
 const Home = () => (
-  <div className="flex flex-col gap-y-16 pb-16">
+  <div className="flex flex-col gap-y-16">
     <Suspense fallback={<CurrentSeasonWorkSkeleton />}>
       <CurrentSeasonWork />
     </Suspense>
