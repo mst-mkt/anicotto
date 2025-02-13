4f19e4823148ff5cb566131253e02483af1494cf
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-13 20:12:07 +0900

    fix: add tooltip for rating select-button in review form

--- src/app/(routes)/works/[workId]/reviews/_components/form/rating-select.tsx
+++ src/app/(routes)/works/[workId]/reviews/_components/form/rating-select.tsx
@@ -2,6 +2,7 @@ import {} from 'lucide-react'
 import type { FC } from 'react'
 import { Label } from '../../../../../../../components/ui/label'
 import { RadioGroup, RadioGroupItem } from '../../../../../../../components/ui/radio-group'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../../../../../../../components/ui/tooltip'
 import { RATING_ICON, RATING_TEXT } from '../../../../../../../constants/rating'
 import { cn } from '../../../../../../../utils/classnames'
 
@@ -53,21 +54,25 @@ export const RatingSelect: FC<RatingSelectProps> = ({
     {label !== undefined && <Label htmlFor={`rating-${name}`}>{label}</Label>}
     <RadioGroup id={`rating-${name}`} name={name} className="flex gap-0 rounded-lg shadow-xs">
       {rating.map((rating) => (
-        <Label
-          key={rating.value}
-          className={cn(
-            '!border-input relative flex w-full shrink cursor-pointer flex-col items-center justify-center gap-2 border p-2 text-sm outline-background-900/10 transition-colors md:flex-row',
-            'first:rounded-s-lg last:rounded-e-lg',
-            'has-[[data-disabled]]:cursor-not-allowed has-[[data-disabled]]:opacity-50',
-            'has-[[data-state=checked]]:z-10',
-            'has-[:focus-visible]:outline-4',
-            rating.className,
-          )}
-        >
-          <RadioGroupItem value={rating.value} className="sr-only" />
-          <rating.icon size={20} className="text-muted-foreground" />
-          {showItemLabel && <span>{rating.label}</span>}
-        </Label>
+        <Tooltip key={rating.value}>
+          <TooltipTrigger asChild={true}>
+            <Label
+              className={cn(
+                '!border-input relative flex w-full shrink cursor-pointer flex-col items-center justify-center gap-2 border p-2 text-sm outline-background-900/10 transition-colors md:flex-row',
+                'first:rounded-s-lg last:rounded-e-lg',
+                'has-[[data-disabled]]:cursor-not-allowed has-[[data-disabled]]:opacity-50',
+                'has-[[data-state=checked]]:z-10',
+                'has-[:focus-visible]:outline-4',
+                rating.className,
+              )}
+            >
+              <RadioGroupItem value={rating.value} className="sr-only" />
+              <rating.icon size={20} className="text-muted-foreground" />
+              {showItemLabel && <span>{rating.label}</span>}
+            </Label>
+          </TooltipTrigger>
+          {!showItemLabel && <TooltipContent>{rating.label}</TooltipContent>}
+        </Tooltip>
       ))}
     </RadioGroup>
   </div>
