1f2b2dd2869c38e73c38444716c3334ee4cb3871
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-15 00:01:12 +0900

    remove: unneed ignore comment

--- src/app/@sidemenu/default.tsx
+++ src/app/@sidemenu/default.tsx
@@ -2,5 +2,4 @@ import { SidemenuLinks } from './_components/links/links'
 
 const Sidemenu = () => <SidemenuLinks />
 
-// biome-ignore lint/style/noDefaultExport: nextjs parallel routes require default export
 export default Sidemenu
