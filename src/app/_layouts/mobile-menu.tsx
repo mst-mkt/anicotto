aaeefc92092db70a2fbf1ecf4c4ea1cf3edc7841
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-15 21:15:33 +0900

    fix: add more padding to bottom of mobile menu

--- src/app/_layouts/mobile-menu.tsx
+++ src/app/_layouts/mobile-menu.tsx
@@ -2,7 +2,7 @@ import { HomeIcon, PenToolIcon, SearchIcon, TelescopeIcon } from 'lucide-react'
 import Link from 'next/link'
 
 export const BottomMenu = () => (
-  <div className="fixed bottom-0 left-0 flex w-full justify-between gap-x-2 rounded-t-xl border border-muted border-b-0 bg-background/64 p-2 backdrop-blur-lg md:hidden">
+  <div className="fixed bottom-0 left-0 flex w-full justify-between gap-x-2 rounded-t-xl border border-muted border-b-0 bg-background/64 p-2 pb-4 backdrop-blur-lg md:hidden">
     <Link
       href="/"
       className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors hover:bg-background-400/10 hover:text-foreground"
