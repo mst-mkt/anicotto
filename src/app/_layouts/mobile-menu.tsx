bb6176fe679e6204b71a89902bbefe626209884f
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-16 20:34:26 +0900

    fix: apply active styles instead of hover

--- src/app/_layouts/mobile-menu.tsx
+++ src/app/_layouts/mobile-menu.tsx
@@ -5,28 +5,28 @@ export const BottomMenu = () => (
   <div className="fixed bottom-0 left-0 flex w-full justify-between gap-x-2 rounded-t-xl border border-muted border-b-0 bg-background/64 p-2 pb-4 backdrop-blur-lg md:hidden">
     <Link
       href="/"
-      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors hover:bg-background-400/10 hover:text-foreground"
+      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors active:bg-background-400/10 active:text-foreground"
     >
       <HomeIcon size={20} />
       <span className="text-xs">ホーム</span>
     </Link>
     <Link
       href="/search"
-      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors hover:bg-background-400/10 hover:text-foreground"
+      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors active:bg-background-400/10 active:text-foreground"
     >
       <SearchIcon size={20} />
       <span className="text-xs">検索</span>
     </Link>
     <Link
       href="/discover"
-      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors hover:bg-background-400/10 hover:text-foreground"
+      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors active:bg-background-400/10 active:text-foreground"
     >
       <TelescopeIcon size={20} />
       <span className="text-xs">探す</span>
     </Link>
     <Link
       href="/track"
-      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors hover:bg-background-400/10 hover:text-foreground"
+      className="flex w-full shrink flex-col items-center gap-y-2 rounded-lg p-2 text-muted-foreground transition-colors active:bg-background-400/10 active:text-foreground"
     >
       <PenToolIcon size={20} />
       <span className="text-xs">記録する</span>
