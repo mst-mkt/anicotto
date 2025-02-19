dd563a4b6a0319906d71e9c120409a93f0b4f623
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-20 01:24:04 +0900

    feat: add user records page (`/users/{username}/records`)

---
+++ src/app/(routes)/users/[username]/(mini-profile)/records/_components/record-list.tsx
@@ -0,0 +1,121 @@
+import { CloudAlertIcon, ImageOffIcon, ScrollTextIcon } from 'lucide-react'
+import Link from 'next/link'
+import type { FC } from 'react'
+import { Image } from '../../../../../../../components/shared/image'
+import { Badge } from '../../../../../../../components/ui/badge'
+import { Skeleton } from '../../../../../../../components/ui/skeleton'
+import type { User } from '../../../../../../../schemas/annict/users'
+import { timeText } from '../../../../../../../utils/time-text'
+import { getUserRecords } from '../get-records'
+import { groupRecords } from './group-records'
+import { RatingBadge } from './rating-badge'
+
+type RecordListProps = {
+  username: User['username']
+}
+
+export const RecordList: FC<RecordListProps> = async ({ username }) => {
+  const records = await getUserRecords(username)
+
+  if (records === null) {
+    return (
+      <div className="flex flex-col items-center gap-y-4 py-16">
+        <CloudAlertIcon size={40} className="text-muted-foreground" />
+        <p>記録の取得に失敗しました</p>
+      </div>
+    )
+  }
+
+  if (records.length === 0) {
+    return (
+      <div className="flex flex-col items-center gap-y-4 py-16">
+        <ScrollTextIcon size={40} className="text-muted-foreground" />
+        <p>記録がありません</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col gap-y-8 py-8">
+      {groupRecords(records).map(({ id, records, work, updatedAt }) => (
+        <div key={id} className="flex gap-x-4">
+          <div className="sticky top-20 aspect-square h-16 w-16 shrink-0 overflow-hidden rounded-md bg-muted">
+            <Image
+              src={work.image}
+              alt={work.title}
+              width={128}
+              height={128}
+              fallback={
+                <div className="flex h-full w-full items-center justify-center bg-muted">
+                  <ImageOffIcon size={24} className="text-muted-foreground" />
+                </div>
+              }
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="flex w-full min-w-0 flex-col gap-y-4">
+            <div className="flex items-center gap-x-2">
+              <Link
+                href={`/works/${work.id}`}
+                className="shrink grow truncate font-bold transition-colors hover:text-anicotto-accent-600 hover:underline"
+              >
+                {work.title}
+              </Link>
+              <time
+                dateTime={updatedAt}
+                className="shrink-0 break-keep text-muted-foreground text-sm"
+              >
+                {timeText(updatedAt)}
+              </time>
+            </div>
+            {records.map(({ id, episode, rating, comment }) => (
+              <div key={id} className="flex flex-col gap-y-2">
+                <Link
+                  href={`/works/${work.id}/episodes/${episode.id}`}
+                  className="group flex items-center gap-x-2"
+                >
+                  <Badge variant="secondary" className="shrink-0 break-keep">
+                    {episode.number}
+                  </Badge>
+                  {episode.title !== null ? (
+                    <span className="min-w-0 grow truncate group-hover:underline">
+                      {episode.title}
+                    </span>
+                  ) : (
+                    <span className="min-w-0 grow truncate text-muted-foreground group-hover:underline">
+                      タイトル不明
+                    </span>
+                  )}
+                  {rating !== null && (
+                    <RatingBadge rating={rating} className="shrink-0 break-keep" />
+                  )}
+                </Link>
+                {comment !== null && comment !== '' && (
+                  <p className="pb-2 text-muted-foreground text-sm">{comment}</p>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const RecordListSkeleton = () => (
+  <div className="flex flex-col gap-y-8 py-8">
+    {[...Array(8)].map((_, i) => (
+      // biome-ignore lint/suspicious/noArrayIndexKey: this is static array
+      <div key={i} className="flex gap-x-4">
+        <Skeleton className="aspect-square h-16 w-16 shrink-0" />
+        <div className="flex w-full flex-col gap-y-4">
+          <div className="flex items-center justify-between gap-x-2">
+            <Skeleton className="h-[1lh] w-1/2" />
+            <Skeleton className="h-[1lh] w-1/4" />
+          </div>
+          <Skeleton className="h-[1lh] w-full" />
+        </div>
+      </div>
+    ))}
+  </div>
+)
---
+++ src/app/(routes)/users/[username]/(mini-profile)/records/get-records.ts
@@ -0,0 +1,83 @@
+import { graphql } from 'gql.tada'
+import { annictGraphqlClient } from '../../../../../../lib/api/graphql'
+import { auth } from '../../../../../../lib/auth'
+import { getValidWorkImage } from '../../../../../../lib/images/valid-url'
+import type { User } from '../../../../../../schemas/annict/users'
+import type { UnNullable, UnPromise } from '../../../../../../types/util-types'
+
+export const getUserRecords = async (username: User['username']) => {
+  await auth()
+
+  const query = graphql(`
+    query getUserRecords($username: String!) {
+      user(username: $username) {
+        records(first: 128, orderBy: { field: CREATED_AT, direction: DESC }) {
+          nodes {
+            annictId
+            ratingState
+            comment
+            updatedAt
+            work {
+              annictId
+              title
+              image {
+                recommendedImageUrl
+                facebookOgImageUrl
+                twitterNormalAvatarUrl
+                twitterAvatarUrl
+              }
+            }
+            episode {
+              annictId
+              numberText
+              title
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const { data, error } = await annictGraphqlClient
+    .query(
+      query,
+      { username },
+      { fetchOptions: { next: { tags: ['records', `records-${username}`] } } },
+    )
+    .toPromise()
+
+  const records = data?.user?.records?.nodes ?? []
+
+  if (records === undefined) {
+    console.error('[/users/[username]/records] Failed to fetch records:', error)
+    return null
+  }
+
+  return await Promise.all(
+    records
+      .filter((record) => record !== null)
+      .map(async (record) => ({
+        id: record.annictId,
+        rating: record.ratingState,
+        comment: record.comment,
+        updatedAt: `${record.updatedAt}`,
+        work: {
+          id: record.work.annictId,
+          title: record.work.title,
+          image: await getValidWorkImage(record.work.annictId.toString(), [
+            record.work.image?.recommendedImageUrl ?? null,
+            record.work.image?.facebookOgImageUrl ?? null,
+            record.work.image?.twitterNormalAvatarUrl ?? null,
+            record.work.image?.twitterAvatarUrl ?? null,
+          ]),
+        },
+        episode: {
+          id: record.episode.annictId,
+          number: record.episode.numberText,
+          title: record.episode.title,
+        },
+      })),
+  )
+}
+
+export type UserRecord = UnNullable<UnPromise<ReturnType<typeof getUserRecords>>>[number]
