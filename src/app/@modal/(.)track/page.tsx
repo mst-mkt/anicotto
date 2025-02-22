e042fd0982aa916e6ba418f82676a3e6073abf4c
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-23 04:23:36 +0900

    fix(styles): add bottom padding to track drawer of pwa (resolve #98)

--- src/app/@modal/(.)track/page.tsx
+++ src/app/@modal/(.)track/page.tsx
@@ -4,7 +4,7 @@ import { TrackFormWrapper } from './_components/form-wrapper'
 
 const TrackModal = () => (
   <BackDialog title="記録する" description="アニメの視聴記録を作成します">
-    <TrackFormWrapper className="flex w-full flex-col gap-y-4">
+    <TrackFormWrapper className="flex w-full flex-col gap-y-4 [@media(display-mode:standalone)]:pb-4">
       <TrackFormContent />
     </TrackFormWrapper>
   </BackDialog>
