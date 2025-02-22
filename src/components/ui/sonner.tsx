da1299063c140cf74fd508d0a26bd4f417821060
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-23 04:44:39 +0900

    fix: prevent overlap of toast and menu in mobile display (resolve #97)

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -3,9 +3,20 @@
 import { useTheme } from 'next-themes'
 import type { FC } from 'react'
 import { Toaster as Sonner, type ToasterProps } from 'sonner'
+import { useMediaQuery } from '../../hooks/useMediaQuery'
 
 export const Toaster: FC<ToasterProps> = (props) => {
   const { theme } = useTheme()
+  const showBottomMenu = useMediaQuery('(width < 48rem)')
 
-  return <Sonner theme={theme === 'dark' ? 'dark' : 'light'} richColors={true} {...props} />
+  return (
+    <Sonner
+      theme={theme === 'dark' ? 'dark' : 'light'}
+      richColors={true}
+      mobileOffset={{
+        bottom: showBottomMenu ? '92px' : '24px',
+      }}
+      {...props}
+    />
+  )
 }
