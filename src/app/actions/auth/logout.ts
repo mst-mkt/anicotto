3862dbe1f8e20f79d8dafa3de1e6623531a77344
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-02-24 04:25:18 +0900

    fix: revalidate viewer info when logout

--- src/app/actions/auth/logout.ts
+++ src/app/actions/auth/logout.ts
@@ -1,7 +1,10 @@
 'use server'
 
+import { revalidateTag } from 'next/cache'
 import { signOut } from '../../../lib/auth'
+import { CACHE_TAGS } from '../../../lib/cache-tag'
 
 export const logoutAction = async () => {
   await signOut({ redirectTo: '/login' })
+  revalidateTag(CACHE_TAGS.ME)
 }
