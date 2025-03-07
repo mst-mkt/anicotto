807dda94c5925fef5dad8997b38b1e0202932556
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-03-07 13:25:00 +0900

    fix: use transition when change resources of serch

--- src/app/(routes)/search/_components/tabs.tsx
+++ src/app/(routes)/search/_components/tabs.tsx
@@ -1,13 +1,17 @@
 'use client'
 
 import { useQueryState } from 'nuqs'
+import { useTransition } from 'react'
 import { cn } from '../../../../utils/classnames'
 import { searchSearchParams } from '../search-params'
 
 export const SearchTabs = () => {
+  const [isPending, startTransition] = useTransition()
   const [resource, setResource] = useQueryState('r', {
     ...searchSearchParams.r,
     defaultValue: 'works',
+    history: 'push',
+    startTransition,
   })
 
   return (
@@ -19,6 +23,7 @@ export const SearchTabs = () => {
           'w-fit cursor-pointer break-keep rounded-md px-3 py-2 font-bold text-muted-foreground text-sm transition-colors hover:text-foreground-300',
           resource === 'works' &&
             'cursor-default bg-background text-foreground shadow-xs hover:text-foreground',
+          resource === 'works' && isPending && 'animate-pulse',
         )}
       >
         作品
@@ -30,6 +35,7 @@ export const SearchTabs = () => {
           'w-fit cursor-pointer break-keep rounded-md px-3 py-2 font-bold text-muted-foreground text-sm transition-colors hover:text-foreground-300',
           resource === 'characters' &&
             'cursor-default bg-background text-foreground shadow-xs hover:text-foreground',
+          resource === 'characters' && isPending && 'animate-pulse',
         )}
       >
         キャラクター
@@ -41,6 +47,7 @@ export const SearchTabs = () => {
           'w-fit cursor-pointer break-keep rounded-md px-3 py-2 font-bold text-muted-foreground text-sm transition-colors hover:text-foreground-300',
           resource === 'people' &&
             'cursor-default bg-background text-foreground shadow-xs hover:text-foreground',
+          resource === 'people' && isPending && 'animate-pulse',
         )}
       >
         人物
@@ -52,6 +59,7 @@ export const SearchTabs = () => {
           'w-fit cursor-pointer break-keep rounded-md px-3 py-2 font-bold text-muted-foreground text-sm transition-colors hover:text-foreground-300',
           resource === 'organizations' &&
             'cursor-default bg-background text-foreground shadow-xs hover:text-foreground',
+          resource === 'organizations' && isPending && 'animate-pulse',
         )}
       >
         団体
