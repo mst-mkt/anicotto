b9bb184f6199b940ecb310d65d025b8354312cbf
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-03-12 07:14:06 +0900

    add: links for track page in side panel of watching works (resolve #140)

--- src/app/@panel/[...routes]/_components/work-item.tsx
+++ src/app/@panel/[...routes]/_components/work-item.tsx
@@ -1,4 +1,4 @@
-import { ImageOffIcon } from 'lucide-react'
+import { ImageOffIcon, PenToolIcon } from 'lucide-react'
 import Link from 'next/link'
 import type { FC } from 'react'
 import { WorkHoverCard } from '../../../../components/hover-card/work/card'
@@ -11,28 +11,36 @@ export const WorkItem: FC<WorkWithStatus> = async (work) => {
 
   return (
     <WorkHoverCard work={work} side="left">
-      <Link
-        href={`/works/${work.id}`}
-        className="flex items-center rounded-md transition-colors hover:bg-muted"
-      >
-        <div className="aspect-square h-12 w-12 shrink-0 overflow-hidden rounded-md">
-          <Image
-            width={128}
-            height={128}
-            src={image}
-            alt={work.title}
-            fallback={
-              <div className="flex h-full w-full items-center justify-center bg-muted text-muted-foreground">
-                <ImageOffIcon size={20} />
-              </div>
-            }
-            className="h-full w-full object-cover"
-          />
-        </div>
-        <div className="p-2">
-          <h3 className="line-clamp-2 text-xs">{work.title}</h3>
-        </div>
-      </Link>
+      <div className="group relative">
+        <Link
+          href={`/works/${work.id}`}
+          className="flex items-center rounded-md transition-colors hover:bg-muted group-hover:pr-12"
+        >
+          <div className="aspect-square h-14 w-14 shrink-0 overflow-hidden rounded-md">
+            <Image
+              width={128}
+              height={128}
+              src={image}
+              alt={work.title}
+              fallback={
+                <div className="flex h-full w-full items-center justify-center bg-muted text-muted-foreground">
+                  <ImageOffIcon size={20} />
+                </div>
+              }
+              className="h-full w-full object-cover"
+            />
+          </div>
+          <div className="px-3 py-2">
+            <h3 className="line-clamp-2 text-xs">{work.title}</h3>
+          </div>
+        </Link>
+        <Link
+          href={`/track/${work.id}`}
+          className="absolute top-2 right-2 hidden aspect-square h-10 items-center justify-center rounded-md border border-background-300 bg-background transition-colors hover:bg-muted group-hover:flex"
+        >
+          <PenToolIcon size={16} className="text-muted-foreground" />
+        </Link>
+      </div>
     </WorkHoverCard>
   )
 }
