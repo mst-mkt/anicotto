98cf1f58573e808e9d1a822e5dadf6a52ffa59d6
mst-mkt <131662659+mst-mkt@users.noreply.github.com>
2025-03-12 06:02:47 +0900

    fix: keep the order of multiple records (resolve #141)

--- src/app/actions/api/create-multiple-records.ts
+++ src/app/actions/api/create-multiple-records.ts
@@ -2,42 +2,47 @@
 
 import { Result } from 'neverthrow'
 import { revalidateTag } from 'next/cache'
+import type { InferOutput } from 'valibot'
 import { annictApiClient } from '../../../lib/api/annict-rest/client'
 import { auth } from '../../../lib/auth'
 import { CACHE_TAGS } from '../../../lib/cache-tag'
 import type { Episode } from '../../../schemas/annict/episodes'
+import type { createRecordResponseSchema } from '../../../schemas/annict/records/api'
 
 export const createMultipleRecords = async (episodeIds: Episode['id'][]) => {
   if (episodeIds.length > 64) return { success: false, error: 'Too many episodes' } as const
 
   await auth()
 
-  const recordResults = Result.combine(
-    await Promise.all(
-      episodeIds.map(async (episodeId) => {
-        const result = await annictApiClient.createRecords(
-          { query: { episode_id: episodeId } },
-          {
-            next: {
-              tags: [
-                CACHE_TAGS.EPISODE(episodeId),
-                CACHE_TAGS.EPISODE_RECORDS(episodeId),
-                CACHE_TAGS.MY_ACTIVITY,
-                CACHE_TAGS.MY_RECORDS,
-              ],
-            },
+  const recordResults = await (async () => {
+    type CreateRecordsResponse = InferOutput<typeof createRecordResponseSchema>
+    const results: Result<CreateRecordsResponse, string>[] = []
+
+    for (const episodeId of episodeIds) {
+      const result = await annictApiClient.createRecords(
+        { query: { episode_id: episodeId } },
+        {
+          next: {
+            tags: [
+              CACHE_TAGS.EPISODE(episodeId),
+              CACHE_TAGS.EPISODE_RECORDS(episodeId),
+              CACHE_TAGS.MY_ACTIVITY,
+              CACHE_TAGS.MY_RECORDS,
+            ],
           },
-        )
+        },
+      )
 
-        if (result.isOk()) {
-          revalidateTag(CACHE_TAGS.EPISODE_RECORDS(episodeId))
-          revalidateTag(CACHE_TAGS.MY_RECORDS)
-        }
+      if (result.isOk()) {
+        revalidateTag(CACHE_TAGS.EPISODE_RECORDS(episodeId))
+        revalidateTag(CACHE_TAGS.MY_RECORDS)
+      }
 
-        return result
-      }),
-    ),
-  )
+      results.push(result)
+    }
+
+    return Result.combine(results)
+  })()
 
   revalidateTag(CACHE_TAGS.MY_LIBRARIES)
   revalidateTag(CACHE_TAGS.MY_ACTIVITY)
