0921888ede34de62b8ac7f1a0b7cf54183a89058
mst-mkt <mail@keito.dev>
2026-02-19 05:36:34 +0900

    init next, hono with turborepo

---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,14 @@
+import type { FC, ReactNode } from "react"
+import "./globals.css"
+
+type RootLayoutProps = {
+  children: ReactNode;
+}
+
+const RootLayout: FC<RootLayoutProps> = ({ children }) => (
+  <html lang="ja">
+    <body>{children}</body>
+  </html>
+)
+
+export default RootLayout
---
+++ apps/web/next.config.ts
@@ -0,0 +1,5 @@
+import type { NextConfig } from "next"
+
+const config = {} satisfies NextConfig
+
+export default config
