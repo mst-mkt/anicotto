0d77f77ab285f769e9bb5c5a838fe36fad22995a
mst-mkt <mail@keito.dev>
2026-02-19 06:08:20 +0900

    init package of design-tokens with terrazzo

---
+++ packages/design-tokens/terrazzo.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "@terrazzo/cli";
+import css from "@terrazzo/plugin-css";
+import tailwind from "@terrazzo/plugin-tailwind";
+
+export default defineConfig({
+  tokens: ["./tokens.jsonc"],
+  outDir: "./dist/",
+  plugins: [
+    css(),
+    tailwind({
+      filename: "tailwind-theme.css",
+      theme: {
+        color: ["color.*"],
+        spacing: ["spacing.*"],
+        radius: ["radius.*"],
+      },
+    }),
+  ],
+});
