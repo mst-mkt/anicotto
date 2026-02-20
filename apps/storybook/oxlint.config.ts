502bda9737e9703dec954e08d70757ef526b3118
mst-mkt <mail@keito.dev>
2026-02-21 03:49:16 +0900

    init oxlint, oxfmt

---
+++ apps/storybook/oxlint.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  plugins: ["react", "react-perf", "jsx-a11y"],
+});
---
+++ apps/web/oxlint.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'oxlint'
+
+export default defineConfig({
+  plugins: ['react', 'react-perf', 'nextjs', 'jsx-a11y'],
+})
