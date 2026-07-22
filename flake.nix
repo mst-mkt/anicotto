def417badf326d5f1e0d87f47e9b1229ce2122b5
mst-mkt <mail@keito.dev>
2026-07-23 00:15:01 +0900

    init: flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "mst-mkt/annix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nix-vite-plus = {
+      url = "github:ryoppippi/nix-vite-plus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nix-vite-plus,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            shellHook = ''
+              export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+              export NIX_SSL_CERT_FILE="$SSL_CERT_FILE"
+              echo "node $(node --version), pnpm $(pnpm --version)"
+            '';
+            packages = [
+              pkgs.nodejs-slim_24
+              (pkgs.pnpm_11.override { nodejs-slim = pkgs.nodejs-slim_24; })
+              nix-vite-plus.packages.${system}.vp
+            ];
+          };
+        }
+      );
+    };
+}
